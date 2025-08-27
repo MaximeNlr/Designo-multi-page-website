d5f82298f1a6079f0ec7acbfe0e8c4bb2bff0d8d
MaximeNlr <158460952+MaximeNlr@users.noreply.github.com>
2025-08-27 14:21:43 +0200

    fix mainLayout import scrollToTop -> ScrollToTop

--- src/layouts/DesignLayout.jsx
+++ src/layouts/DesignLayout.jsx
@@ -3,7 +3,7 @@ import Header from "../components/header/Header"
 import Navigation from "../components/navigation/Navigation"
 import GetInTouch from "../components/get_in_touch/GetInTouch"
 import Footer from "../components/footer/Footer"
-import ScrollToTop from "../utils/scrollToTop"
+import ScrollToTop from "../utils/ScrollToTop"
 import { Outlet } from "react-router-dom"
 
 export default function DesignLayout() {
