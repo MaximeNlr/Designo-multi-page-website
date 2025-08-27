09921f0794bc6ee7c9b8413f13e3392fd2245c5e
MaximeNlr <158460952+MaximeNlr@users.noreply.github.com>
2025-08-27 14:18:14 +0200

    fix + update scrollToTop

--- src/layouts/DesignLayout.jsx
+++ src/layouts/DesignLayout.jsx
@@ -3,12 +3,13 @@ import Header from "../components/header/Header"
 import Navigation from "../components/navigation/Navigation"
 import GetInTouch from "../components/get_in_touch/GetInTouch"
 import Footer from "../components/footer/Footer"
-import ScrollToTop from "../utils/scrollToTop";
+import ScrollToTop from "../utils/scrollToTop"
 import { Outlet } from "react-router-dom"
 
 export default function DesignLayout() {
     return (
         <main>
+            <ScrollToTop />
             <Header />
             <MobileHeader />
             <Outlet />
